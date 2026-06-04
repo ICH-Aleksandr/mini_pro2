401409599fb63023be7e0b15611d92629105b6fd
Aleksandr <ichmarchuk@gail.com>
2026-06-04 13:27:55 +0200

    full version

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import cartRoutes from "./routes/cartRoutes.js";
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use("/api/cart", cartRoutes);
 
 app.listen(port, () => {
   connectDB();
